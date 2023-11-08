f48b0ac1b6e3d51bf741bba032ca11ec29697397
juns98 <juns98@naver.com>
2023-11-08 18:41:14 +0900

    feat: update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+dotenv.config();
 
+export const otherOptions = {
+  upgradeable: true,
+};
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  paths: {
+    artifacts: "./artifacts",
+  },
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
